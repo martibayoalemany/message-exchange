a0216c1ce7e15ef897495c8ac62ccf962cd1ee0c
Marti Bayo Alemany <martibayoalemany@gmail.com>
2018-04-29 01:09:03 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -4,7 +4,7 @@ mvn compile
 mvn jar:jar
 
 echo " ------ InProc Starter ------ "
-time java -cp target/360t-1.0-SNAPSHOT.jar com.graphai.InProcStarter
+time java -cp target/mmmm-SNAPSHOT.jar com.graphai.InProcStarter
 echo " ------ OutProc Starter ------ "
-time java -cp target/360t-1.0-SNAPSHOT.jar com.graphai.OutProcStarter
+time java -cp target/mmm-SNAPSHOT.jar com.graphai.OutProcStarter
 
